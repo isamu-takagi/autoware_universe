fe9a29e3a4921b971af7b39844a94a572f012855
Takagi, Isamu <isamu.takagi@tier4.jp>
2026-04-16 19:35:38 +0900

    add status publish
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- system/autoware_system_mode_tools/python/widget.py
+++ system/autoware_system_mode_tools/python/widget.py
@@ -12,28 +12,76 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from autoware_system_mode_msgs.msg import SystemModeStatus
 from autoware_system_mode_msgs.msg import TrajectorySource
 from python_qt_binding import QtWidgets
 from tier4_system_msgs.msg import CommandSourceStatus
 
+from .utils import default_qos
 from .utils import durable_qos
 
 
 class MainWidget(QtWidgets.QWidget):
     def __init__(self, node):
         super().__init__()
-
+        modes = [
+            (1, "Stop"),
+            (2, "Autonomous"),
+        ]
+        self.modes_control = DrivingModeControl(node, modes)
         self.status_trajectory = TrajectoryGateDisplay(node)
         self.status_command = CommandGateDisplay(node)
 
-        layout = QtWidgets.QHBoxLayout()
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.modes_control)
         layout.addWidget(self.status_trajectory)
         layout.addWidget(self.status_command)
         self.setLayout(layout)
 
 
-class DrivingModeControl(QtWidgets.QLabel):
-    pass
+class DrivingModeControl(QtWidgets.QWidget):
+    def __init__(self, node, modes):
+        super().__init__()
+        self.buttons = {}
+
+        self.create_widget(modes)
+        self.timer = node.create_timer(0.5, self.on_timer)
+        self.publisher = node.create_publisher(
+            SystemModeStatus, "/system/driving_mode/status", default_qos(1)
+        )
+
+    def on_msg(self, msg):
+        print(msg.source)
+
+    def on_timer(self):
+        self.publish()
+
+    def on_clicked(self, mode, status):
+        self.publish()
+
+    def publish(self):
+        msg = SystemModeStatus()
+        self.publisher.publish(msg)
+
+    def create_widget(self, modes):
+        layout = QtWidgets.QGridLayout()
+        self.setLayout(layout)
+        for row, (mode, name) in enumerate(modes):
+            layout.addWidget(QtWidgets.QLabel(f"{name} ({mode})"), row, 0)
+            self.create_button(mode, layout, row)
+
+    def create_button(self, mode, layout, row):
+        button_none = QtWidgets.QPushButton("None")
+        button_true = QtWidgets.QPushButton("True")
+        button_false = QtWidgets.QPushButton("False")
+        button_group = QtWidgets.QButtonGroup(self)
+        buttons = [button_none, button_true, button_false]
+        for col, button in enumerate(buttons, start=1):
+            button_group.addButton(button)
+            button.setCheckable(True)
+            button.clicked.connect(self.on_clicked)
+            layout.addWidget(button, row, col)
+        self.buttons[mode] = buttons
 
 
 class TrajectoryGateDisplay(QtWidgets.QLabel):
