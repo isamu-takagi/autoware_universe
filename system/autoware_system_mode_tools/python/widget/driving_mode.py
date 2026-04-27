2e8e886585263453a4258ae1899dbaac6bddd1f6
Takagi, Isamu <isamu.takagi@tier4.jp>
2026-04-27 14:12:43 +0900

    add all control button
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- system/autoware_system_mode_tools/python/widget/driving_mode.py
+++ system/autoware_system_mode_tools/python/widget/driving_mode.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections import defaultdict
 
 from autoware_system_mode_msgs.msg import SystemModeStatus
 from autoware_system_mode_msgs.msg import SystemModeStatusItem
@@ -31,6 +32,7 @@ class DrivingModeControl(QtWidgets.QWidget):
         super().__init__()
         self.clock = node.get_clock()
         self.status = {}
+        self.buttons = defaultdict(dict)
 
         self.create_widget(modes)
         self.timer = node.create_timer(0.5, self.on_timer)
@@ -56,12 +58,15 @@ class DrivingModeControl(QtWidgets.QWidget):
         layout = QtWidgets.QGridLayout()
         layout.setSpacing(0)
         layout.setRowStretch(len(modes) + 1, 1)
-        layout.addWidget(QtWidgets.QLabel("Autoware Mode"), 0, 0)
-        layout.addWidget(centered_label("Continuable"), 0, 1, 1, 3)
-        layout.addWidget(centered_label("Available"), 0, 4, 1, 3)
-        layout.addWidget(centered_label("Stable"), 0, 7, 1, 3)
+        layout.addWidget(QtWidgets.QLabel("Autoware Mode"), 1, 0)
+        layout.addWidget(centered_label("Continuable"), 1, 1, 1, 3)
+        layout.addWidget(centered_label("Available"), 1, 4, 1, 3)
+        layout.addWidget(centered_label("Stable"), 1, 7, 1, 3)
         self.setLayout(layout)
-        for row, (mode, name) in enumerate(modes, start=1):
+        self.create_all_buttons(SystemModeStatusItem.CONTINUABLE, layout, 0, 1)
+        self.create_all_buttons(SystemModeStatusItem.AVAILABLE, layout, 0, 4)
+        self.create_all_buttons(SystemModeStatusItem.STABLE, layout, 0, 7)
+        for row, (mode, name) in enumerate(modes, start=2):
             layout.addWidget(QtWidgets.QLabel(f"{name} ({mode})"), row, 0)
             self.create_button(SystemModeStatusItem.CONTINUABLE, mode, layout, row, 1)
             self.create_button(SystemModeStatusItem.AVAILABLE, mode, layout, row, 4)
@@ -69,6 +74,12 @@ class DrivingModeControl(QtWidgets.QWidget):
 
     def set_status(self, mode, category, status):
         self.status[(mode, category)] = status
+        self.publish()
+
+    def set_all_status(self, category, status):
+        for mode, button in self.buttons[(category, status)].items():
+            button.setChecked(True)
+            self.status[(mode, category)] = status
         self.publish()
 
     def create_button(self, category, mode, layout, row, col):
@@ -83,4 +94,18 @@ class DrivingModeControl(QtWidgets.QWidget):
         for index, button in enumerate(buttons):
             button_group.addButton(button)
             button.setCheckable(True)
+            layout.addWidget(button, row, col + index)
+        self.buttons[(category, None)][mode] = button_none
+        self.buttons[(category, True)][mode] = button_true
+        self.buttons[(category, False)][mode] = button_false
+
+    def create_all_buttons(self, category, layout, row, col):
+        button_none = QtWidgets.QPushButton("None")
+        button_true = QtWidgets.QPushButton("True")
+        button_false = QtWidgets.QPushButton("False")
+        button_none.clicked.connect(lambda: self.set_all_status(category, None))
+        button_true.clicked.connect(lambda: self.set_all_status(category, True))
+        button_false.clicked.connect(lambda: self.set_all_status(category, False))
+        buttons = [button_none, button_true, button_false]
+        for index, button in enumerate(buttons):
             layout.addWidget(button, row, col + index)
