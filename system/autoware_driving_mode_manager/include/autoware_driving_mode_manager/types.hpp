5dbf9327ca93184b1fbccfcc79f326e8dc8c7d7e
Takagi, Isamu <isamu.takagi@tier4.jp>
2026-06-03 13:15:46 +0900

    rename mrm behavior request
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- system/autoware_driving_mode_manager/include/autoware_driving_mode_manager/types.hpp
+++ system/autoware_driving_mode_manager/include/autoware_driving_mode_manager/types.hpp
@@ -86,11 +86,11 @@ enum class MrmStrategy {
 
 struct RequestModes
 {
-  AutowareMode operation_mode;   // Operation mode API
-  PlatformMode platform_mode;    // Operation mode API
-  MrmStrategy mrm_strategy;      // MRM request API
-  AutowareMode mrm_target_mode;  // MRM request API
-  AutowareMode autoware_mode;    // Decision logic plugin.
+  AutowareMode operation_mode;  // Operation mode API
+  PlatformMode platform_mode;   // Operation mode API
+  MrmStrategy mrm_strategy;     // MRM request API
+  AutowareMode mrm_behavior;    // MRM request API
+  AutowareMode autoware_mode;   // Decision logic plugin.
 };
 
 using AutowareModeSet = std::unordered_set<AutowareMode>;
--- system/autoware_driving_mode_manager/src/core/manager.cpp
+++ system/autoware_driving_mode_manager/src/core/manager.cpp
@@ -56,7 +56,7 @@ Manager::Manager(ManagerInit & init)
   request_.operation_mode = config_->to_autoware_mode(OperationMode::kStop);
   request_.platform_mode = PlatformMode::kUnknown;
   request_.mrm_strategy = MrmStrategy::kNone;
-  request_.mrm_target_mode = unknown_mode;
+  request_.mrm_behavior = unknown_mode;
   request_.autoware_mode = unknown_mode;
 }
 
