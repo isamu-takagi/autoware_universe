19f354aecce047bb01f4795a5e431067b95cf46e
Takagi, Isamu <isamu.takagi@tier4.jp>
2026-06-19 21:55:44 +0900

    add test template
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

---
+++ system/autoware_driving_mode_manager/test/utils/mock.hpp
@@ -0,0 +1,26 @@
+// Copyright 2026 The Autoware Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef UTILS__MOCK_HPP_
+#define UTILS__MOCK_HPP_
+
+#include "type/interface.hpp"
+
+bool foo();
+
+class MockInterface : public autoware::driving_mode_manager::Interface
+{
+};
+
+#endif  // UTILS__MOCK_HPP_
